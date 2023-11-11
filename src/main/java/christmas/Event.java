21bacd1b9e32a05e4526319a47189a6e850778fa
heukwu <yoo4068@gmail.com>
2023-11-11 09:35:22 +0900

    Feat: 평일 할인

--- src/main/java/christmas/Event.java
+++ src/main/java/christmas/Event.java
@@ -1,5 +1,8 @@
 package christmas;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Event {
 
     private static final int EVENT_START_DATE = 1;
@@ -7,6 +10,7 @@ public class Event {
     private static final int CHRISTMAS_D_DAY_EVENT_END_DATE = 25;
     private static final int D_DAY_DISCOUNT_PRICE = 1_000;
     private static final int D_DAY_DISCOUNT_INCREASE_PRICE = 100;
+    public static int weekdayDessertDiscountPrice = 2_023;
 
     public static int christmasDDayDiscount(int price, int date) {
         if (date < EVENT_START_DATE || date > EVENT_END_DATE) {
@@ -17,5 +21,22 @@ public class Event {
         }
 
         return D_DAY_DISCOUNT_PRICE + D_DAY_DISCOUNT_INCREASE_PRICE * (date - 1);
+    }
+
+    public static int weekdayDiscount(int date, int dessertCount) {
+        if (date < EVENT_START_DATE || date > EVENT_END_DATE) {
+            throw new IllegalArgumentException("[error] 유효하지 않은 날짜입니다. 다시 입력해주세요.");
+        }
+        if (isWeekday(date)) {
+            return dessertCount * weekdayDessertDiscountPrice;
+        }
+
+        return 0;
+    }
+
+    private static boolean isWeekday(int date) {
+        DayOfWeek dayOfWeek = LocalDate.of(2023, 12, date).getDayOfWeek();
+
+        return dayOfWeek != DayOfWeek.FRIDAY && dayOfWeek != DayOfWeek.SATURDAY;
     }
 }
--- src/test/java/christmas/EventTest.java
+++ src/test/java/christmas/EventTest.java
@@ -27,4 +27,24 @@ class EventTest {
             int discountPrice = Event.christmasDDayDiscount(50_000, 33);
         }).isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    @DisplayName("입력받은 날짜가 평일이면 디저트 개수만큼 할인을 받는다.")
+    public void weekdayDiscount() {
+        assertThat(Event.weekdayDiscount(5, 1)).isEqualTo(2_023);
+    }
+
+    @Test
+    @DisplayName("입력받은 날짜가 평일이 아니면 할인을 받지 못한다.")
+    public void notWeekdayDiscount() {
+        assertThat(Event.weekdayDiscount(1, 1)).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("유요한 날짜를 입력하지 않았을 때 예외가 발생한다.")
+    public void invalidDateWeekday() {
+        assertThatThrownBy(() -> {
+            int discountPrice = Event.weekdayDiscount(50, 1);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
